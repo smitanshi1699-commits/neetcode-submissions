6c7384493b7ab036b6c22f755c748af7327567f2
smitanshi1699-commits <smitanshi1699@gmail.com>
2026-06-25 19:40:00 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.py
@@ -0,0 +1,24 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, val: int) -> None:
+        if not self.stack:
+            self.stack.append((val,val))
+        else:
+            previous_min = self.stack[-1][1]
+            current_min = min(val,previous_min)
+            self.stack.append((val,current_min))
+
+    def pop(self) -> None:
+        if self.stack:
+            self.stack.pop()
+
+    def top(self) -> int:
+        if self.stack:
+            return self.stack[-1][0]
+
+    def getMin(self) -> int:
+        if self.stack:
+            return self.stack[-1][1]
