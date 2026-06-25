4ec6fea686de72e95cfd1589a984eaac75f91f7b
smitanshi1699-commits <smitanshi1699@gmail.com>
2026-06-25 15:14:01 +0530

    Add: validate-parentheses - submission-4

---
+++ Data Structures & Algorithms/validate-parentheses/submission-4.py
@@ -0,0 +1,44 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        a = list(s)
+        my_stack= []
+        if a[0] == ')' or a[0] == '}' or a[0] == ']':
+            return False
+        for i in a:
+            if i == ')':
+                if len(my_stack) > 0 and '(' == my_stack[-1]:
+                    my_stack.pop()
+                else:
+                    return False
+            elif i == '}':
+                if len(my_stack) > 0 and '{' == my_stack[-1]:
+                    my_stack.pop()
+                else:
+                    return False
+            elif i == ']':
+                if len(my_stack) > 0 and '[' == my_stack[-1]:
+                    my_stack.pop()
+                else:
+                    return False
+            else :
+                my_stack.append(i)
+        if len(my_stack) == 0:
+            return True
+        else:
+            return False
+
+
+
+# if a[n] == ')':
+#     if '(' in stack:
+#         stack.remove('(')
+# elif a[n] == ']':
+#     if '[' in stack:
+#         stack.remove('[')
+# elif a[n] == '}':
+#     if '{' in stack:
+#         stack.remove('{')
+# else:
+#     stack.append(a[n])
+
+# if stack.is_empty()
